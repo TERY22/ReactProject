015dca251e4e1d0ac6284537aed9750c4cea1c42
TERY <fire_jacky@mail.ru>
2021-02-26 22:25:29 +0300

    Complete

---
+++ src/components/app-header/app-header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './app-header.css';
+
+const AppHeader = ({liked, allPost}) => {
+   return (
+      <div className="app-header d-flex">
+         <h1>Tariel Yusupzahnov</h1>
+         <h2>{allPost} записей, из них понравилось {liked}</h2>
+      </div>
+   )
+}
+
+export default AppHeader;
